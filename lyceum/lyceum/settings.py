1c97b21da183fb4a7a14036deb3fb08dfa1bff5a
Ann Shevchenko <annshev1220@gmail.com>
2024-10-08 21:59:14 +0300

    добавлен django-debug-toolbar

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "homepage",
     "catalog",
     "about",
+    "debug_toolbar",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -40,6 +41,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = "lyceum.urls"
--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from . import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,3 +9,6 @@ urlpatterns = [
     path("catalog/", include("catalog.urls")),
     path("about/", include("about.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += (path("__debug__/", include("debug_toolbar.urls")),)
