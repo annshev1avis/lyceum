acc7eb2b6a7feb9f580da2f668223f71e55d5d3e
Ann Shevchenko <annshev1220@gmail.com>
2024-10-16 23:06:04 +0300

    Squash migrations и добавлены help_text в поля моделей

--- lyceum/catalog/models.py
+++ lyceum/catalog/models.py
@@ -38,11 +38,13 @@ class Item(core.models.CoreModel):
         on_delete=django.db.models.CASCADE,
         related_name="items",
         verbose_name="категория",
+        help_text="Выберите категорию",
     )
     tags = django.db.models.ManyToManyField(
         "Tag",
         related_name="items",
         verbose_name="теги",
+        help_text="Можно выбрать несколько тегов",
     )
 
     class Meta:
@@ -54,7 +56,13 @@ class Item(core.models.CoreModel):
 
 
 class Tag(core.models.CoreModel):
-    slug = django.db.models.SlugField("слаг", max_length=200, unique=True)
+    slug = django.db.models.SlugField(
+        "слаг",
+        max_length=200,
+        unique=True,
+        help_text="Слаг может состоять из латинских букв, "
+        "цифр и нижнего подчеркивания",
+    )
 
     class Meta:
         verbose_name = "тег"
@@ -65,11 +73,19 @@ class Tag(core.models.CoreModel):
 
 
 class Category(core.models.CoreModel):
-    slug = django.db.models.SlugField("слаг", max_length=200, unique=True)
+    slug = django.db.models.SlugField(
+        "слаг",
+        max_length=200,
+        unique=True,
+        help_text="Слаг может состоять из латинских букв, "
+        "цифр и нижнего подчеркивания",
+    )
+
     weight = django.db.models.IntegerField(
         "вес",
         default=100,
         validators=[is_int_1_32767],
+        help_text="Введите число от 1 до 32767",
     )
 
     class Meta:
--- lyceum/core/models.py
+++ lyceum/core/models.py
@@ -3,8 +3,17 @@ import django.db
 
 class CoreModel(django.db.models.Model):
     id = django.db.models.BigAutoField("id", primary_key=True)
-    name = django.db.models.CharField("название", max_length=150, unique=True)
-    is_published = django.db.models.BooleanField("опубликовано", default=True)
+    name = django.db.models.CharField(
+        "название",
+        max_length=150,
+        unique=True,
+        help_text="Введите уникальное имя",
+    )
+    is_published = django.db.models.BooleanField(
+        "опубликовано",
+        default=True,
+        help_text="Элемент будет виден пользователям",
+    )
 
     class Meta:
         abstract = True
