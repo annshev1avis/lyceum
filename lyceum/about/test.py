e3ec5b5f81b1e3e7bfd5ac95d4e793f8d72d923d
Ann Shevchenko <annshev1220@gmail.com>
2024-10-24 21:04:28 +0300

    Тесты catalog разбиты на модули

--- lyceum/about/test.py
+++ lyceum/about/test.py
@@ -5,5 +5,3 @@ class AboutTests(django.test.TestCase):
     def test_about(self):
         response = django.test.Client().get("/about/")
         self.assertEqual(response.status_code, 200)
-        content = str(response.content, encoding="utf-8")
-        self.assertEqual(content, "О проекте")
--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -7,8 +7,6 @@ class HomepageTests(django.test.TestCase):
     def test_homepage_endpoint(self):
         response = django.test.Client().get("/")
         self.assertEqual(response.status_code, 200)
-        str_content = str(response.content, encoding="utf-8")
-        self.assertEqual(str_content, "Главная")
 
     def test_teapot_endpoint(self):
         response = django.test.Client().get("/coffee/")
