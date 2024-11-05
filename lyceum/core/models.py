6e88515f5fe848af113876c9e41614c6221c41ed
Ann Shevchenko <annshev1220@gmail.com>
2024-11-05 13:30:15 +0300

    При добавлении элементов реализуется дополнительная проверка на уникальность имени

--- lyceum/core/models.py
+++ lyceum/core/models.py
@@ -1,8 +1,15 @@
+import re
+
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.safestring import mark_safe
 from sorl.thumbnail import get_thumbnail
+import transliterate
 
 __all__ = ["CoreModel", "ImageModel"]
+
+
+ONLY_LETTERS_REGEX = re.compile(r"[^\w]")
 
 
 class ManagerActive(models.Manager):
@@ -18,13 +25,41 @@ class CoreModel(models.Model):
         "название",
         max_length=150,
         unique=True,
-        help_text="Введите уникальное имя",
+        help_text="Введите уникальное название",
     )
     is_published = models.BooleanField(
         "опубликовано",
         default=True,
         help_text="Элемент будет виден пользователям",
     )
+    canonical_name = models.CharField(
+        "каноническое название",
+        max_length=150,
+        unique=True,
+        null=True,
+        editable=False,
+        help_text="Каноническое название элемента",
+    )
+
+    def _generate_canonical_name(self):
+        transliterated = transliterate.translit(
+            self.name.lower(),
+            reversed=True,
+        )
+        return ONLY_LETTERS_REGEX.sub("", transliterated)
+
+    def save(self, *args, **kwargs):
+        self.canonical_name = self._generate_canonical_name()
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        if (
+            type(self)
+            .objects.filter(canonical_name=self.canonical_name)
+            .exclude(id=self.id)
+            .exists()
+        ):
+            raise ValidationError("Объект с таким именем уже существует")
 
     class Meta:
         abstract = True
