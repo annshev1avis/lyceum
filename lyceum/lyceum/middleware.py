1beefa90dec4098a3d02dda6682a7d5f81e1919e
Ann Shevchenko <annshev1220@gmail.com>
2024-11-08 18:07:41 +0300

    изменен regex в ReverseMiddleware

--- lyceum/lyceum/middleware.py
+++ lyceum/lyceum/middleware.py
@@ -1,17 +1,19 @@
+import re
+
 import django.conf
 
 __all__ = ["ReverseWordsMiddleware", "reverse_rus_word"]
 
 
 REVERSE_TIME = 10
-RUSSIAN_LETTERS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+ONLY_RUSSIAN_REGEX = re.compile(r"^\s*[\W_]*[А-Яа-яЁё]+[\W_]*\s*$")
 
 
 def reverse_rus_word(word):
     if word == "":
         return word
 
-    return word[::-1] if word[0].lower() in RUSSIAN_LETTERS else word
+    return word[::-1] if ONLY_RUSSIAN_REGEX.match(word) else word
 
 
 class ReverseWordsMiddleware:
@@ -20,19 +22,27 @@ class ReverseWordsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def __call__(self, request):
-        ReverseWordsMiddleware.count += 1
+    @classmethod
+    def check_should_reverse(cls):
+        if django.conf.settings.ALLOW_REVERSE not in (True, None):
+            return False
 
+        cls.count += 1
+        if cls.count == 10:
+            cls.count = 0
+            return True
+
+        return False
+
+    def __call__(self, request):
         response = self.get_response(request)
 
-        if (
-            django.conf.settings.ALLOW_REVERSE
-            or django.conf.settings.ALLOW_REVERSE is None
-        ) and ReverseWordsMiddleware.count % REVERSE_TIME == 0:
-            words = response.content.decode("utf-8").split()
-            response.content = " ".join(map(reverse_rus_word, words)).encode(
-                "utf-8",
-            )
-            ReverseWordsMiddleware.count = 0
+        if not self.check_should_reverse():
+            return response
+
+        words = response.content.decode("utf-8").split()
+        response.content = " ".join(map(reverse_rus_word, words)).encode(
+            "utf-8",
+        )
 
         return response
